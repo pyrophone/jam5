bddcbdaf1bc615fc3fac83faee0b5b5d33887645
Frisk Jiang <728501875@qq.com>
2019-02-23 17:22:37 -0500

    Create a simple flood & collectable coins

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour {
+
+    private bool _isCollected;
+    
+	// Use this for initialization
+	void Start () {
+        _isCollected = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    protected virtual void OnCollect() {
+        _isCollected = true;
+        gameObject.SetActive(false);
+    }
+
+    private void OnCollisionEnter(Collision other) {
+        if (other.gameObject.tag == "Player") {
+            OnCollect();
+        }
+    }
+
+}
---
+++ Assets/Scripts/Flood.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flood : MonoBehaviour {
+    
+    public float playerSpeedRefactor;
+
+    [SerializeField] private float _moveSpeed;
+    [SerializeField] private float _floodTime;
+
+    [SerializeField] private Transform _startPoint;
+    [SerializeField] private Transform _endPoint;
+    
+	// Use this for initialization
+	void Start() {
+		
+	}
+	
+	// Update is called once per frame
+	void Update() {
+        if (currentTime < _floodTime) {
+            Moving();
+        }  
+    }
+
+    private float currentTime = 0;
+    private void Moving() {
+        currentTime += Time.deltaTime;
+        transform.position = Vector3.Lerp(_startPoint.position, _endPoint.position, currentTime / _floodTime);
+    }
+
+}
